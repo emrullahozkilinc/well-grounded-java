ff4604bf21c2dbce83d9dc98299cf231833308fc
emrullahozkilinc <emr.ozk.71@gmail.com>
2024-02-04 14:12:19 +0300

    noclassdeferr

---
+++ src/main/java/com/emr/wellgroundedjava/WellGroundedJavaApplication.java
@@ -0,0 +1,13 @@
+package com.emr.wellgroundedjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellGroundedJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellGroundedJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emr/wellgroundedjava/WellGroundedJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emr.wellgroundedjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellGroundedJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
